23510d39ef944e143181264feb19107cab85c662
Ashraful Islam <19103189@iubat.edu>
2024-07-10 01:06:26 +0600

    [DONE] all section design and funcationality with role based login

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -26,9 +26,10 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const baseApi = createApi({
   reducerPath: "baseApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "https://assignment-6-backend-relief-goods.vercel.app",
+    baseUrl:
+      "https://l2-b2-frontend-path-assignment-6-server-starter-pack-main.vercel.app/",
     credentials: "include",
   }),
-  tagTypes: ["supplies", "reliefs", "projects"],
+  tagTypes: ["supplies", "reliefs", "projects","login"],
   endpoints: () => ({}),
 });
